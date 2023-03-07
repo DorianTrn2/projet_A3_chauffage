ccba5d51400d1f02eebac057dfbd434a6cb65127
Dorian Tarin <dorian.tarin@isen-ouest.yncrea.fr>
2023-03-07 10:27:22 +0100

    regulation

--- code/main.c
+++ code/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "consigne.h"
 #include "autotests.h"
+#include "regulation.h"
 
 int main(){
     /*remove(".verrouConsigne");
@@ -10,8 +11,28 @@ int main(){
         a = consigne(a);//on récupère la commande en fonction de l
         printf("cmd : %f\n",a);
     }*/
-    float b = testConsigne();
-    printf("%f", b);
+
+    /*float b = testConsigne();
+    printf("%f", b);*/
+
+    //printf("%f",testRegulationTOR());
+    printf("\n%f", testRegulationPID());
+    /*int nT = 3;
+    float tab[3] = {12,13,14};
+    float test = regulationTest(1,10,&tab,nT);
+    printf("%f", test);*/
+
+    /*temp_t temperature;
+    temperature.exterieure = 14.0;
+    temperature.interieure = tabT[nT-1];
+    struct simParam_s*  monSimulateur_ps = simConstruct(temperature); // creation du simulateur, puissance intialisée à 0%
+    //int i=0; // increment de boucle
+    //float puissance = 70.0; // puissance de chauffage
+    //for(i=0;i< 30;i++){
+    temperature = simCalc(cmd,monSimulateur_ps); // simulation de l'environnement
+    //}
+    simDestruct(monSimulateur_ps); // destruction de simulateur
+    return temperature.interieure;*/
 
     //BLOC DE TEST
     /*float score1=0,score2=0,score3=0,score4=0,score5=0;
--- code/regulation.c
+++ code/regulation.c
@@ -1,32 +1,49 @@
 #include "regulation.h"
+float regulation(int regul,float consigne,float T,float* oldT, float* oldI);
+
 
 float regulationTest(int regul,float consigne,float* tabT, int nT){
-    int chauffage=0;
+    float reg;
+    float oldT = 98272390;
+    float oldI = 0;
+    for(int i=0;i<nT;i++){
+        reg = regulation(regul, consigne, tabT[i], &oldT, &oldI);
+    }
+    //printf("This is final reg : %f",reg);
+    return reg;
+}
+
+
+float regulation(int regul,float consigne,float T,float* oldT, float* oldI){
+    float cmd;
     if(regul == 1){ // TOR
-        if(consigne<tabT[nT-1]){
+        *oldI = 0;
+        if(consigne>T){
             cmd=40;
         }
         else{
             cmd=0;
         }
-
-
-        //if(chauffage){
-        //    temp_t temp;
-        //    temp.exterieure = 15;
-        //    temp.interieure = 15;
-        //}
-
     }
     else{ // PID
+        float Kp=1,Ki=0.0967,Kd=0.1, dt=10;
 
+        //P
+        float e = consigne - T;
+        float P = Kp*e;
 
+        //I
+        float e2 = T - consigne;
+        e2 = (e2>0) ? e2 : -e2;
+        float I = *oldI + Ki*(e2*dt);
 
+        //D
+        float D = Kd * (T- *oldT )/dt;
 
+        float PID = I+P+D;
+        *oldT = T;
+        *oldI = I;
+        return PID;
     }
-    float cmd;
-
-    return cmd;
 }
-
 
